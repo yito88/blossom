8a0fc7d125350e97f4bd0f0f3af701ccc906d2e5
yito88 <yuji@phact-columba.com>
2020-05-05 14:33:37 +0900

    tag v1.0.0

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject blossom "0.1.0-SNAPSHOT"
-  :description "Simple Bloom filter"
+(defproject blossom "1.0.0"
+  :description "Simple Bloom Filter"
   :url "https://github.com/yito88/blossom"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.taoensso/nippy "2.14.0"]]
-  :repl-options {:init-ns blossom.core})
+                 [com.taoensso/nippy "2.14.0"]])
